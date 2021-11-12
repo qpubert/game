193fbd1147970ef618ebf274e91d626bdd9d98a9
Quentin Pubert <quentin.pubert@outlook.fr>
2021-11-12 20:39:05 +0100

    now parameterizing window from TilemapEditor

--- apps/tilemap_editor.cpp
+++ apps/tilemap_editor.cpp
@@ -2,8 +2,14 @@
 
 #include "tilemap_editor_app/tilemap_editor_state.hpp"
 
+using namespace sf;
 using namespace std;
 
 TilemapEditor::TilemapEditor() : Application("Tilemap editor") {
+  auto const desktopMode = VideoMode::getDesktopMode();
+
+  setFullscreen(false, false);
+  resize({desktopMode.width / 2, desktopMode.height / 2}, false);
+
   stateStack_.pushState(make_unique<TilemapEditorState>(*this));
 }
