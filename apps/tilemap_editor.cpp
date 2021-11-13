5b6cd6611fdbd95aef41bc1ce5dfccb186285e67
Quentin Pubert <quentin.pubert@outlook.fr>
2021-11-13 16:35:52 +0100

    now TilemapEditor sets resizable to true

--- apps/tilemap_editor.cpp
+++ apps/tilemap_editor.cpp
@@ -9,6 +9,7 @@ TilemapEditor::TilemapEditor() : Application("Tilemap editor") {
   auto const desktopMode = VideoMode::getDesktopMode();
 
   setFullscreen(false, false);
+  setResizable(true, false);
   resize({desktopMode.width / 2, desktopMode.height / 2}, false);
 
   stateStack_.pushState(make_unique<TilemapEditorState>(*this));
